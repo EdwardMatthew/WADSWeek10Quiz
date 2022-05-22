8673102de218a15d915099af572f50cc00654b22
Edward Matthew <edward.kurniawan@binus.ac.id>
2022-05-23 03:17:59 +0700

    finally finished (pls send help)

--- React/src/pages/CreatePost.jsx
+++ React/src/pages/CreatePost.jsx
@@ -1,6 +1,61 @@
-import 'react' from React.Component
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
-export default class CreatePost extends React.Component {
-	const	
+function CreatePost() {
+	let navigate = useNavigate();
 
+  const [name, setName] = useState("");
+  const [alias, setAlias] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:8000/heroes/", {
+        method: "POST",
+				headers: {
+					"Content-Type": 'application/json',
+				},
+        body: JSON.stringify({
+          name: name,
+          alias: alias
+        }),
+      });
+      if (res.status === 201) {
+        setName("");
+        setAlias("");
+        setMessage("Hero created and posted!");
+				return navigate("/");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alias}
+          placeholder="Alias"
+          onChange={(e) => setAlias(e.target.value)}
+        />
+
+        <button type="submit">Post</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
+  );
 }
+
+export default CreatePost;
